c86a048b3d2563fd0a84ba25fe21b7cd0c3ec815
vchatchai <ee56054@gmail.com>
2018-10-10 17:12:26 +0700

    add asyncawait

---
+++ globals/asyncawait/readdir.js
@@ -0,0 +1,25 @@
+const {join} = require('path');
+const {promisify} = require('util');
+
+const fs = require('fs');
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
+
+
+async function readDir (dir, acc= []){
+    await Promise.all(
+        (await readdir(dir)).map(async file => {
+            file = join(dir, file);
+            return (await stat(file)).isDirectory() && acc.push(file) && readDir(file, acc);
+        }) 
+    )
+
+
+    return acc
+}
+
+readDir(`./`).then(dirInfo => {
+    for ( let v of dirInfo ) {  
+        console.log(v)
+    }
+});
