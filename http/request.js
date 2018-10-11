695bdcdd21abb62ea1d79df8255b9c3a042e30f9
vchatchai <ee56054@gmail.com>
2018-10-11 11:39:34 +0700

    add http request

---
+++ http/request.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+http.request({
+    host:'www.example.org',
+    method: 'GET',
+    path: "/"
+
+}, function(response){
+    response.setEncoding('utf8')
+    response.on("readable", ()=> {
+        console.log(response.read())
+    })
+}).end()
